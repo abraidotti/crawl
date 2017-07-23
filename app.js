b7540bfe0490313a062194b5f210e73619a57428
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-07-23 10:00:09 -0400

    add south command

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const example = require("./example.js");
 const room1 = require("./rooms/room1.js").room1;
 const room2 = require("./rooms/room2.js").room2;
 
-const rooms = {"starting_room":room1, "smooth_corridor":room2};
+const rooms = {"starting_room":room1, 
+				"smooth_corridor":room2
+			};
 
 let current_room = room1;
 
@@ -24,7 +26,8 @@ const commands = {
     "help": function(){print("I respond to the following commands: " + (Object.keys(this)).join(", ") + "\n");},
     "test":  example.example,
 
-    "north":  go_north, 
+    "north":  go_north,
+    "south": go_south
 
 };
 
@@ -39,6 +42,18 @@ function go_north(){
 		var new_room_name = current_room.exits.north;
 		print("arrived.\n");
 		var new_room = rooms["smooth_corridor"];
+		current_room = new_room;
+	}
+}
+
+function go_south(){
+	var exits = Object.keys(current_room.exits);
+	print("Going south.\n");
+	print(exits + "\n");
+	if (exits.includes("south")){
+		var new_room_name = current_room.exits.south;
+		print("arrived.\n");
+		var new_room = rooms["starting_room"];
 		current_room = new_room;
 	}
 }
